0c804220f9bc92ac0512bf95b4dc80202d86aebd
Enzo Pita <phy.enzo@gmail.com>
2023-06-19 01:04:48 -0300

    feat(config): create database config file

---
+++ src/config/database.ts
@@ -0,0 +1,36 @@
+import type { Options } from '@mikro-orm/core';
+
+import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
+import { Logger } from '@nestjs/common';
+import { registerAs } from '@nestjs/config';
+
+const logger = new Logger('MikroORM');
+
+export const databaseConfig = registerAs('database', () => {
+  return {
+    type: 'postgresql',
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT, 10),
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    dbName: process.env.DATABASE_NAME,
+    debug: process.env.NODE_ENV !== 'production',
+    logger: logger.log.bind(logger),
+    highlighter: new SqlHighlighter(),
+    driverOptions: {
+      connection: {
+        ssl: process.env.DATABASE_SSL === 'true',
+      },
+    },
+    entities: ['dist/**/*.entity.js'],
+    entitiesTs: ['src/**/*.entity.ts'],
+    migrations: {
+      path: 'dist/migrations',
+      pathTs: 'src/migrations',
+    },
+    seeder: {
+      path: 'dist/seeders',
+      pathTs: 'src/seeders',
+    },
+  } as Options;
+});
